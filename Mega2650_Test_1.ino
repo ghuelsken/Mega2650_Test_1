5c89611b944eb47e916eccd5a153b1877b32295d
Hülsken Gregor <ghuelsken@hotmail.de>
2020-08-02 18:33:47 +0200

    Projektdateien hinzufügen.

---
+++ Mega2650_Test_1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Mega2650_Test_1.ino
+ Created:	02.08.2020 18:32:55
+ Author:	ghuelsken
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
